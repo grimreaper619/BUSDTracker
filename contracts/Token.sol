821a6c22b7afa8cc4a7e52293fc801ab505d4919
grimreaper619 <grimreapergit619@gmail.com>
2021-12-25 03:37:43 +0530

    BUSD Tracker finish

---
+++ contracts/Token.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
+import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract BUSDTracker is ERC20, Ownable {
+    IUniswapV2Router02 internal constant uniswapV2Router =
+        IUniswapV2Router02(0x9Ac64Cc6e4415144C455BD8E4837Fea55603e5c3);
+    address public uniswapV2Pair;
+    uint256 private constant initSupply = 100 * 1e9;
+
+    constructor() ERC20("BUSD Tracker", "BUSDT") {
+        _mint(tx.origin, initSupply);
+
+        uniswapV2Pair = IUniswapV2Factory(uniswapV2Router.factory()).createPair(
+                address(this),
+                uniswapV2Router.WETH()
+            );
+    }
+
+    function mint(address _to, uint256 _amount) public onlyOwner {
+        _mint(_to, _amount);
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return 9;
+    }
+}
